ff147ca146546da014e3a805cd4c1a700df079fa
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:37:59 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// dpma-gebuehren - DPMA Gebuehren - Gebuehrenrechner und Kostenueberblick fuer DPMA Anmeldungen. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
